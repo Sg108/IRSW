7fc9dd1aa6f54746271ead074d2d459760d770cd
Siddhant Kushwaha <siddhant.kushwaha@gmail.com>
2022-11-26 22:17:41 +0530

    optimised algorithm added performance comparisons

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,96 +1,100 @@
-import React, {useState,useEffect} from 'react'
-import words from '../files/dictionary_compact.json'
-import EditDistanceOptimised from '../utils/EditDistanceOptimised'
+import React, { useState, useEffect } from "react"
+import words from "../files/dictionary_compact.json"
+// import wordsFull from "../files/dictionary.json"
+import {
+    editDistanceOptimised,
+    editDistanceOptimum,
+} from "../utils/EditDistanceOptimised"
+import editDistance from "../utils/EditDistance"
 const SearchBar = () => {
+    const [searchInput, setSearchInput] = useState("")
+    const [list, setList] = useState([...Object.keys(words)])
+    // const [fullList, setFullList] = useState([...Object.keys(wordsFull)])
+    const [press, setPress] = useState(false)
+    const [display, setDisplay] = useState([])
 
- const [searchInput, setSearchInput] = useState("");
- const [list, setList] = useState([...Object.keys(words)]);
- const [press,setPress]=useState(false)
- const [display, setDisplay] = useState([]);
+    //  let w=[]
+    //  for(let i =0 ;i<10000;i++)
+    //  {
+    //      w.push(list[i])
+    //  }
 
-//  let w=[]
-//  for(let i =0 ;i<10000;i++)
-//  {
-//      w.push(list[i])
-//  }
+    useEffect(() => {
+        // console.log(list)
+        // console.log(fullList)
+        if (searchInput.length > 0) {
+            let d = []
 
-  useEffect(() => {
-    
-   
-    //console.log(w)
-    console.log(list)
-    if(searchInput.length>0)
-    {
-        let d=[]
-        list.forEach((word)=>{
-            d.push([word,EditDistanceOptimised(searchInput,word)])
-        })
-    // let d=list.filter((word) => {
-    //     return word.includes(searchInput);
-    // }).slice(0,5)
-    // d=d.sort(function (a, b) {
-    //     return a.value<b.value;
-    // });
-    d=d.sort(function(a,b){
-       
-        return a[1]-b[1]
-    })
-    d=d.slice(0,5)
-    console.log(d)
-    setDisplay(d)
-}
-   else {
-    setDisplay([])
+            let startTime = performance.now()
+            list.forEach((word) => {
+                editDistanceOptimised(searchInput, word)
+            })
+            let endTime = performance.now()
+            console.log(
+                "time elapsed (optimised): ",
+                endTime - startTime,
+                " ms"
+            )
+            startTime = performance.now()
+            list.forEach((word) => {
+                editDistance(searchInput, word)
+            })
+            endTime = performance.now()
+            console.log(
+                "time elapsed (unoptimised) : ",
+                endTime - startTime,
+                " ms"
+            )
+            startTime = performance.now()
+            list.forEach((word) => {
+                editDistanceOptimum(searchInput, word)
+            })
+            endTime = performance.now()
+            console.log(
+                "time elapsed (super optimised): ",
+                endTime - startTime,
+                " ms"
+            )
+
+            d = d.sort(function (a, b) {
+                return a[1] - b[1]
+            })
+            d = d.slice(0, 100)
+            // console.log(d)
+            setDisplay(d)
+        } else {
+            setDisplay([])
+        }
+        console.log(display)
+    }, [press])
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        setSearchInput(e.target.value)
     }
-    console.log(display)
-    
-   
-},[press]);
-
-
-const handleChange = (e) => {
-  e.preventDefault();
-  setSearchInput(e.target.value);
- 
-};
-const handlePress = (e) =>{
-    if(e.key==='Enter')
-    {
-      setPress(!press)
-   
+    const handlePress = (e) => {
+        if (e.key === "Enter") {
+            setPress(!press)
+        }
     }
+
+    return (
+        <div>
+            <input
+                type="search"
+                placeholder="Search here"
+                onChange={handleChange}
+                onKeyDown={handlePress}
+                value={searchInput}
+            />
+
+            <h2>Matches</h2>
+
+            {display.map((x, i) => {
+                return <div>{x[0]}</div>
+            })}
+        </div>
+    )
 }
 
-
-
-
-
-return <div>
-
-<input
-   type="search"
-   placeholder="Search here"
-   onChange={handleChange}
-   onKeyDown={handlePress}
-   value={searchInput} />
-
-    <h2>Matches</h2>
-   
-
-
-{display.map((x,i) => {
-
-
-   return <div>{x[0]}</div>
-   
-
-
-})}
-
-
-</div>
-
-
-};
-
-export default SearchBar;
+export default SearchBar
--- src/utils/EditDistanceOptimised.js
+++ src/utils/EditDistanceOptimised.js
@@ -1,26 +1,42 @@
 const min = (arr) => {
-  let mine = 1e7;
-  for (let i of arr) {
-    if (i < mine) mine = i;
-  }
-  return mine;
-};
-
-const EditDistanceOptimised = (str1, str2) => {
-  let n1 = str1.length;
-  let n2 = str2.length;
-  let prev = new Array(n2 + 1).fill(0);
-  for (let i = 1; i <= n2; i++) prev[i] = i;
-  for (let i = 1; i <= n1; i++) {
-    let curr = new Array(n2 + 1).fill(0);
-    curr[0] = i;
-    for (let j = 1; j <= n2; j++) {
-      if (str1[i - 1] == str2[j - 1]) curr[j] = prev[j - 1];
-      else curr[j] = 1 + min([curr[j - 1], prev[j], prev[j - 1]]);
+    let mine = 1e7
+    for (let i of arr) {
+        if (i < mine) mine = i
     }
-    prev = curr;
-  }
-  return prev[n2];
-};
+    return mine
+}
 
-export default EditDistanceOptimised;
+export const editDistanceOptimised = (str1, str2) => {
+    let n1 = str1.length
+    let n2 = str2.length
+    let prev = new Array(n2 + 1).fill(0)
+    for (let i = 1; i <= n2; i++) prev[i] = i
+    for (let i = 1; i <= n1; i++) {
+        let curr = new Array(n2 + 1).fill(0)
+        curr[0] = i
+        for (let j = 1; j <= n2; j++) {
+            if (str1[i - 1] == str2[j - 1]) curr[j] = prev[j - 1]
+            else curr[j] = 1 + min([curr[j - 1], prev[j], prev[j - 1]])
+        }
+        prev = curr
+    }
+    return prev[n2]
+}
+
+export const editDistanceOptimum = (str1, str2) => {
+    let n = str1.length
+    let m = str2.length
+    let dp = new Array(m + 1).fill(0)
+    for (let i = 1; i <= m; i++) dp[i] = i
+    for (let i = 1; i <= n; i++) {
+        let dist = i - 1
+        dp[0] = i
+        for (let j = 1; j <= m; j++) {
+            let temp = dp[j]
+            if (str1[i - 1] == str2[j - 1]) dp[j] = dist
+            else dp[j] = 1 + min([dist, dp[j], dp[j - 1]])
+            dist = temp
+        }
+    }
+    return dp[m]
+}
